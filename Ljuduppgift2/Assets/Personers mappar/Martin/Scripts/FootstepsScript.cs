58a252737a01366c7c141459c713bd0d568ac692
F0RGEMASTER9001 <117176044+F0RGEMASTER9001@users.noreply.github.com>
2023-08-31 12:35:53 +0200

    Put scene together

---
+++ Ljuduppgift2/Assets/Personers mappar/Martin/Scripts/FootstepsScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootstepsScript : MonoBehaviour
+{
+    [SerializeField] private CharacterController player;
+    [SerializeField] private GameObject footStepSoundsSource;
+
+    private void Update()
+    {
+        Debug.Log(player.velocity.z);
+
+        if (player.velocity.z > 0.1 || player.velocity.z < -0.1 || player.velocity.x > 0.1 || player.velocity.x < -0.1)
+        {
+            footStepSoundsSource.SetActive(true);
+            Debug.Log("Jag går faktiskt");
+        }
+        else if (player.velocity.z == 0)
+        {
+            footStepSoundsSource.SetActive(false);
+        }
+    }
+}
